45036991003ffd6866049c8ea8c2527770213ab5
aicodingtf3 <aicodingtf3@gmail.com>
2025-05-22 11:48:18 +0530

    Add files via upload

---
+++ MediaPlayerWrapper.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace TizenMediaPlayer
+{
+    public class MediaPlayer : IDisposable
+    {
+        private IntPtr _handle;
+
+        [DllImport("tizen_media_player", EntryPoint = "mp_create")]
+        private static extern IntPtr mp_create();
+
+        [DllImport("tizen_media_player", EntryPoint = "mp_destroy")]
+        private static extern void mp_destroy(IntPtr player);
+
+        [DllImport("tizen_media_player", EntryPoint = "mp_set_url")]
+        private static extern void mp_set_url(IntPtr player, string url);
+
+        [DllImport("tizen_media_player", EntryPoint = "mp_play")]
+        private static extern void mp_play(IntPtr player);
+
+        [DllImport("tizen_media_player", EntryPoint = "mp_pause")]
+        private static extern void mp_pause(IntPtr player);
+
+        [DllImport("tizen_media_player", EntryPoint = "mp_seek")]
+        private static extern void mp_seek(IntPtr player, int positionMs);
+
+        public MediaPlayer()
+        {
+            _handle = mp_create();
+        }
+
+        public void SetUrl(string url) => mp_set_url(_handle, url);
+        public void Play() => mp_play(_handle);
+        public void Pause() => mp_pause(_handle);
+        public void Seek() => mp_seek(_handle, positionMs);
+
+        public void Dispose()
+        {
+            if (_handle != IntPtr.Zero)
+            {
+                mp_destroy(_handle);
+                _handle = IntPtr.Zero;
+            }
+			GC.SuppressFinalize(this);
+        }
+		
+		~MediaPlayer()
+		{
+				Dispose();
+		}
+    }
+}
